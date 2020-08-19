276084c9f064d59b0f4a028cc8913cc45241e269
Jae Young Choi <jaeyoungnsb@gmail.com>
2020-08-19 18:08:46 -0400

    Updates

---
+++ precache-manifest.c9f3cdcb42169499272f57f8089aba79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732e8bd67dc02855badf7c23cd560d2d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c87168c147d0badb2e1a",
+    "url": "/todo/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "75c846b0083953c2b4bc",
+    "url": "/todo/static/css/main.3cb33b9b.chunk.css"
+  },
+  {
+    "revision": "c87168c147d0badb2e1a",
+    "url": "/todo/static/js/2.4c71f1ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.4c71f1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c846b0083953c2b4bc",
+    "url": "/todo/static/js/main.ad6cc543.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
